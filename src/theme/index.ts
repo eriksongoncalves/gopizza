1bf8ebe097d3338c46736ca8feca26e012579d44
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2023-12-22 10:19:57 -0300

    feat: config styled theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -8,5 +8,25 @@ export default {
         regular: 'DMSerifDisplay_400Regular'
       }
     }
+  },
+  colors: {
+    background: '#F7F7F7',
+
+    primary_900: '',
+    primary_800: '#E03F50',
+    primary_100: '#D16470',
+    primary_50: '#FFABB3',
+
+    secondary_900: '#528F33',
+    secondary_50: '#F7FFF9',
+
+    alert_900: '#B27F00',
+    alert_800: '#C5941A',
+    alert_50: '#F3EFE5',
+
+    shape: '#DCDCDC',
+    title: '#FFFFFF',
+
+    GRADIENT: ['#B83341', '#E03F50']
   }
 } as const;
